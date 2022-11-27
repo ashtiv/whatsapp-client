ed412ddc8913c34bd406fc3410a5c5bc4bac976d
ashtiv <ashishtiwari05032002@gmail.com>
2022-11-27 23:33:03 +0530

    Add all my files

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://whatsapp-server-alpha.vercel.app/"
+    baseURL: "https://whatsapp-server-alpha.vercel.app/"
 });
 export default instance;
